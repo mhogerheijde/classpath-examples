fbedc86c9aed5661dfac3b78dd568c92fc9f6d36
Matthias Hogerheijde <matthias@hogerheijde.net>
2018-12-08 23:38:41 +0100

    Extract inputStream to String [version 5]

--- module1/src/net/hogerheijde/Runnable.java
+++ module1/src/net/hogerheijde/Runnable.java
@@ -1,20 +1,13 @@
 package net.hogerheijde;
 
 import java.io.InputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
 
 class Runnable {
 	public static void main(String[] args) {
 		System.out.println("Module 1");
 
 		InputStream inputStream = Runnable.class.getResourceAsStream("/header.txt");
-		String headerText = inputStreamToStringWithLoadsOfAssumptions(inputStream);
+		String headerText = Util.inputStreamToStringWithLoadsOfAssumptions(inputStream);
 		System.out.println(headerText);
-	}
-
-	public static String inputStreamToStringWithLoadsOfAssumptions(InputStream in) {
-		return new BufferedReader(new InputStreamReader(in)).lines().collect(Collectors.joining("\n"));
 	}
 }
---
+++ module1/src/net/hogerheijde/Util.java
@@ -0,0 +1,13 @@
+package net.hogerheijde;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.util.stream.Collectors;
+
+
+class Util {
+	public static String inputStreamToStringWithLoadsOfAssumptions(InputStream in) {
+  		return new BufferedReader(new InputStreamReader(in)).lines().collect(Collectors.joining("\n"));
+	}
+}
