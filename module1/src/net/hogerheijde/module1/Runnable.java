caa8c6528f33c383e8b4fbff91a30b9644906776
Matthias Hogerheijde <matthias@hogerheijde.net>
2018-12-10 14:49:02 +0100

    Showing the resource URL [version 10]

--- module1/src/net/hogerheijde/module1/Runnable.java
+++ module1/src/net/hogerheijde/module1/Runnable.java
@@ -8,6 +8,8 @@ class Runnable {
 	public static void main(String[] args) {
 		System.out.println("Module 1");
 
+		System.out.println(String.format("Reading file from %s", Runnable.class.getResource("header.txt")));
+
 		InputStream inputStream = Runnable.class.getResourceAsStream("header.txt");
 		String headerText = Util.inputStreamToStringWithLoadsOfAssumptions(inputStream);
 		System.out.println(headerText);
